e62cebba33c27edbf10c39e6e293cf56a4636d3e
Jesse Garcia <jgarc269@bolt.cs.ucr.edu>
2020-03-02 13:19:35 -0800

    done

--- application.cpp
+++ application.cpp
@@ -99,6 +99,16 @@ GLuint loadTarga(const char* filename)
     //glBindTexture
     //glTexParameteri
     //glTexImage2D
+   
+    glGenTextures(1, &texture_id);
+    glBindTexture(GL_TEXTURE_2D , texture_id);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 
     return texture_id;
 }
